7c6417e487cce31742aca20fdbd5abd52ac33601
Athrv08 <globalxpose@gmail.com>
2025-03-12 17:38:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let text = "EduSelf Academy is Coming Soon...";
-    let i = 0;
-    function typeWriter() {
-        if (i < text.length) {
-            document.getElementById("typing-text").innerHTML += text.charAt(i);
-            i++;
-            setTimeout(typeWriter, 100);
-        }
+// Hide preloader after loading
+window.onload = function() {
+    setTimeout(() => {
+        document.getElementById("preloader").style.display = "none";
+        document.getElementById("main-content").classList.remove("hidden");
+    }, 2000);
+};
+
+// Toggle Side Menu
+function toggleMenu() {
+    var menu = document.getElementById("side-menu");
+    if (menu.style.right === "0px") {
+        menu.style.right = "-250px";
+    } else {
+        menu.style.right = "0px";
     }
-    typeWriter();
-});
+}
